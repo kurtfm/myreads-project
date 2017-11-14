b6b8415c651cf57b3ee10a9aa3d457252857e39e
Kurt Moeller <kurtfm@gmail.com>
2017-11-14 12:23:45 -0800

    bugfix: handle the case when user navigates to a path there are no views for

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import './App.css'
 import BookShelf from './BookShelf'
 import Search from './Search'
@@ -28,29 +28,43 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="App">
-        <Route exact path='/' render={() => (
-          <BookShelf
-            shelfNames={SHELF_NAMES}
-            currentBooks={this.state.currentBooks}
-            updateCurrentBooks={this.getCurrentBooks}
-          />
-        )}/>
-        <Route path='/search/:term?' render={(props,history) => (
-          <Search
-            {...props}
-            shelfNames={SHELF_NAMES}
-            currentBooks={this.state.currentBooks}
-            updateCurrentBooks={this.getCurrentBooks}
-          />
-        )}/>
-        <Route path='/book/:id' render={(props,history) => (
-          <BookView
-            {...props}
-            shelfNames={SHELF_NAMES}
-            currentBooks={this.state.currentBooks}
-          />
-        )}/>
-
+        <Switch>
+          <Route exact path='/' render={() => (
+            <BookShelf
+              shelfNames={SHELF_NAMES}
+              currentBooks={this.state.currentBooks}
+              updateCurrentBooks={this.getCurrentBooks}
+            />
+          )}/>
+          <Route path='/search/:term?' render={(props,history) => (
+            <Search
+              {...props}
+              shelfNames={SHELF_NAMES}
+              currentBooks={this.state.currentBooks}
+              updateCurrentBooks={this.getCurrentBooks}
+            />
+          )}/>
+          <Route path='/book/:id' render={(props,history) => (
+            <BookView
+              {...props}
+              shelfNames={SHELF_NAMES}
+              currentBooks={this.state.currentBooks}
+            />
+          )}/>
+          <Route path='*' exact={true} render={() => (
+            <div>
+              <div className="missing-view">
+                Sorry! The page you are looking for can't be found.
+                Here are your book shelves instead:
+              </div>
+              <BookShelf
+                shelfNames={SHELF_NAMES}
+                currentBooks={this.state.currentBooks}
+                updateCurrentBooks={this.getCurrentBooks}
+              />
+            </div>
+          )}/>
+        </Switch>
       </div>
     );
   }
